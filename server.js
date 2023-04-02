ed4d06a7cfd682b2bb19702a358cd49e2a062e64
neoneb <87101137+neoneb@users.noreply.github.com>
2023-04-02 12:06:38 -0700

    debug

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send('it is working!');
+  res.send(db.users);
 });
 app.post('/signin', signin.handleSignin(db, bcrypt));
 app.post('/register', (req, res) => {
